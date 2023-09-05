a43d12e15f7e166e37cd67d988770fe55adb1444
Wineshuga <uzochukwuwinnie@gmail.com>
2023-09-05 23:37:00 +0100

    Create and add location component

---
+++ src/components/Location.js
@@ -0,0 +1,21 @@
+import { PropTypes } from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { NavLink } from 'react-router-dom';
+import { fetchLocationData } from '../redux/weather/weatherSlice';
+
+const Location = ({ name }) => {
+  const dispatch = useDispatch();
+  return (
+    <NavLink to={`/${name}`} onClick={() => dispatch(fetchLocationData(name))}>
+      <div>
+        { name }
+      </div>
+    </NavLink>
+  );
+};
+
+Location.propTypes = {
+  name: PropTypes.string.isRequired,
+};
+
+export default Location;
