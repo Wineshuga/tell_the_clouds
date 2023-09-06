38aa5de55b78eb2c96368e2326112e21c584be0a
Wineshuga <uzochukwuwinnie@gmail.com>
2023-09-06 15:22:52 +0100

    Style view location page

--- src/components/ViewLocation.js
+++ src/components/ViewLocation.js
@@ -1,5 +1,7 @@
 import { useSelector } from 'react-redux';
 import { Link, useParams } from 'react-router-dom';
+import styles from '../styles/ViewLocation.module.css';
+import nigeria from '../images/nigeria.png';
 
 const ViewLocation = () => {
   const { name } = useParams();
@@ -7,22 +9,38 @@ const ViewLocation = () => {
 
   return (
     <section>
-      <Link to="/"><h1>TellTheClouds</h1></Link>
-      <h1>
-        Weather in
-        {' '}
-        { name}
-      </h1>
-      <section>
+      <div className={styles.header}>
+        <Link to="/">
+          <div className={styles.nav}>
+            <img
+              src={nigeria}
+              alt="nigeria map"
+              className={styles.nigeria_map}
+            />
+            <h1>TellTheClouds</h1>
+          </div>
+          {' '}
+        </Link>
+      </div>
+      <div className={styles.weatherBox}>
+        <h1>
+          Weather in
+          {' '}
+          { name}
+        </h1>
+      </div>
+      <section className={styles.weatherDetails}>
         <p>{weather.text}</p>
         <p>
           Temperature:
           {' '}
           {weather.minTemp}
+          F
           {' '}
           -
           {' '}
           {weather.maxTemp}
+          F
         </p>
         <p>{(weather.dayPrecipitation) ? 'Day Time: Expect rain' : 'Day Time: No rain'}</p>
         <p>{(weather.nightPrecipitation) ? 'Night Time: Expect rain' : 'Night Time: No rain'}</p>
