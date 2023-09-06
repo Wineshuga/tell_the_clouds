fde95464c8720d89b12775def1d7f36c09beb4d3
Wineshuga <uzochukwuwinnie@gmail.com>
2023-09-06 14:56:44 +0100

    Add general stylings

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './styles/App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import ViewLocation from './components/ViewLocation';
