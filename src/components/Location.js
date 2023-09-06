afd210a2a089c2df6e213b4b78a089cde4c65910
Wineshuga <uzochukwuwinnie@gmail.com>
2023-09-06 14:57:31 +0100

    Add styles to location links

--- src/components/Location.js
+++ src/components/Location.js
@@ -1,13 +1,14 @@
 import { PropTypes } from 'prop-types';
 import { useDispatch } from 'react-redux';
 import { NavLink } from 'react-router-dom';
+import styles from '../styles/Location.module.css';
 import { fetchLocationData } from '../redux/weather/weatherSlice';
 
 const Location = ({ name }) => {
   const dispatch = useDispatch();
   return (
     <NavLink to={`/${name}`} onClick={() => dispatch(fetchLocationData(name))}>
-      <div>{name}</div>
+      <div className={styles.location}><p>{name}</p></div>
     </NavLink>
   );
 };
